0dc477359fe69786546f348f95e98e264d22a68d
Jae-Hong-Kim <newfly101@naver.com>
2024-06-04 14:14:38 +0900

    [가계부 프로젝트] [1] InputMoney 입력 요소 입력

--- src/component/Money/InputMoney.js
+++ src/component/Money/InputMoney.js
@@ -3,8 +3,25 @@ import classes from './InputMoney.module.css';
 
 const InputMoney = () => {
     return (
-        <div>
-            <div className={classes.mainBox}>InputMoney</div>
+        <div className={classes.mainBox}>
+            <label htmlFor="productName">이름</label>
+            <input id="productName" name="productName"/>
+            <label htmlFor="productPrice">가격</label>
+            <input id="productPrice" name="productPrice"/>
+            <label htmlFor="productType">유형</label>
+            <select id="productType" name="productType"/>
+            <label htmlFor="productBuyDate">구입 날짜</label>
+            <input type="date" id="productBuyDate" name="productBuyDate"/>
+            <label htmlFor="productMemo">메모</label>
+            <input type="checkbox" id="productMemo" name="productMemo"/>
+            <label htmlFor="productMemoDescription">메모 작성</label>
+            <input id="productMemoDescription" name="productMemoDescription"/>
+            <label htmlFor="productReBuy">재구매 의사</label>
+            <input type="radio" id="productReBuying" name="productReBuying"/>
+            <label htmlFor="productReBuying">한다</label>
+            <input type="radio" id="productReBuying" name="productReBuying"/>
+            <label htmlFor="productReBuying">안한다</label>
+
         </div>
     );
 };
