598f847aad27b293bcf146c4361e957463d1107d
Jae-Hong-Kim <newfly101@naver.com>
2024-06-04 15:12:57 +0900

    [가계부 프로젝트] [1] InputMoney 입력 요소 CSS및 VIEW 수정하기

--- src/component/Money/InputMoney.js
+++ src/component/Money/InputMoney.js
@@ -4,24 +4,59 @@ import classes from './InputMoney.module.css';
 const InputMoney = () => {
     return (
         <div className={classes.mainBox}>
-            <label htmlFor="productName">이름</label>
-            <input id="productName" name="productName"/>
-            <label htmlFor="productPrice">가격</label>
-            <input id="productPrice" name="productPrice"/>
-            <label htmlFor="productType">유형</label>
-            <select id="productType" name="productType"/>
-            <label htmlFor="productBuyDate">구입 날짜</label>
-            <input type="date" id="productBuyDate" name="productBuyDate"/>
-            <label htmlFor="productMemo">메모</label>
-            <input type="checkbox" id="productMemo" name="productMemo"/>
-            <label htmlFor="productMemoDescription">메모 작성</label>
-            <input id="productMemoDescription" name="productMemoDescription"/>
-            <label htmlFor="productReBuy">재구매 의사</label>
-            <input type="radio" id="productReBuying" name="productReBuying"/>
-            <label htmlFor="productReBuying">한다</label>
-            <input type="radio" id="productReBuying" name="productReBuying"/>
-            <label htmlFor="productReBuying">안한다</label>
-
+            <div className={classes.boxComponent}>
+                <div className={classes.labelBox}>
+                    <label htmlFor="productName">이름</label>
+                </div>
+                <div className={classes.elementBox}>
+                    <input id="productName" name="productName"/>
+                </div>
+            </div>
+            <div className={classes.boxComponent}>
+                <div className={classes.labelBox}>
+                    <label htmlFor="productPrice">가격</label>
+                </div>
+                <div className={classes.elementBox}>
+                    <input id="productPrice" name="productPrice"/>
+                </div>
+            </div>
+            <div className={classes.boxComponent}>
+                <div className={classes.labelBox}>
+                    <label htmlFor="productType">유형</label>
+                </div>
+                <div className={classes.elementBox}>
+                    <select id="productType" name="productType"/>
+                </div>
+            </div>
+            <div className={classes.boxComponent}>
+                <div className={classes.labelBox}>
+                    <label htmlFor="productBuyDate">구입 날짜</label>
+                </div>
+                <div className={classes.elementBox}>
+                    <input type="date" id="productBuyDate" name="productBuyDate"/>
+                </div>
+            </div>
+            <div className={classes.boxComponent}>
+                <div className={classes.labelBox}>
+                    <label htmlFor="productMemo">메모</label>
+                </div>
+                <div className={classes.elementBox}>
+                    <input type="checkbox" id="productMemo" name="productMemo"/>
+                    <label>메모 작성</label>
+                    <input id="productMemoDescription" name="productMemoDescription"/>
+                </div>
+            </div>
+            <div className={classes.boxComponent}>
+                <div className={classes.labelBox}>
+                    <label htmlFor="productReBuy1">재구매 의사</label>
+                </div>
+                <div className={classes.elementBox}>
+                    <input type="radio" id="productReBuy1" name="productReBuy"/>
+                    <label htmlFor="productReBuy1">한다</label>
+                    <input type="radio" id="productReBuy2" name="productReBuy"/>
+                    <label htmlFor="productReBuy2">안한다</label>
+                </div>
+            </div>
         </div>
     );
 };
