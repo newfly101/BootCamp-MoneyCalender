3e7739708df8e9c3c546db078506aa085d7ddbc3
Jae-Hong-Kim <newfly101@naver.com>
2024-06-05 10:55:26 +0900

    [가계부 프로젝트] [3] Filter 입력 요소 추가

--- src/component/Money/Filter.js
+++ src/component/Money/Filter.js
@@ -4,7 +4,44 @@ import classes from './filter.module.css';
 const Filter = () => {
     return (
         <div className={classes.mainBox}>
-            Filter
+            <div className={classes.boxComponent}>
+                <select
+                    id="productType"
+                    name="productType"
+                >
+                    <option id="productTypeName" name="productTypeName" value="productTypeName">유형 필터</option>
+                </select>
+                <select
+                    id="productType"
+                    name="productType"
+                >
+                    <option id="productTypeName" name="productTypeName" value="productTypeName">정렬 기준</option>
+                </select>
+                <div className={classes.labelBox}>
+                    <label htmlFor="productBuyDate">시작 기간</label>
+                </div>
+                <div className={classes.elementBox}>
+                    <input type="date"
+                           id="productBuyDate"
+                           name="productBuyDate"
+                           // max={today}
+                           // value={productDate}
+                           // onChange={onChangeProductDate}
+                    />
+                </div>
+                <div className={classes.labelBox}>
+                    <label htmlFor="productBuyDate">끝 기간</label>
+                </div>
+                <div className={classes.elementBox}>
+                    <input type="date"
+                           id="productBuyDate"
+                           name="productBuyDate"
+                           // max={today}
+                           // value={productDate}
+                           // onChange={onChangeProductDate}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
